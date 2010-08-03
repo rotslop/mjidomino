703ec152dda6d7c669bdbeb82d78ea2aecb5a0e5
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-03 14:18:50 +0000

    Esta carpeta es el proyecto de verano de Programacion y Algoritmos III

---
+++ ProyectoVerano/src/proyectoverano/ListaUsuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectoverano;
+
+/**
+ *
+ * @author Minerva Verdugo Ruiz
+ */
+public class ListaUsuario {
+Usuario elusuario [];
+
+
+public void OrdenarNumPartida (Usuario U){
+
+}
+}
