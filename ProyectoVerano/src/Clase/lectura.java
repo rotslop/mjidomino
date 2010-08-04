b316022f0db27d212b17766713424ebfd732fdf6
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-04 18:28:34 +0000

    Estas son las clases en el manejo de archivo XML

--- ProyectoVerano/src/Clase/lectura.java
+++ ProyectoVerano/src/Clase/lectura.java
@@ -14,25 +14,56 @@ import java.io.*;
 class lectura { //lectura de los datos del Usuario
 public static void main(String[]args)throws IOException {
 
-BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
-String nombre, apellido, nickname;
-int clave;
-//JCalendar fechNacimiento;
-
-System.out.println("Bienvenidos a IDomino");
-System.out.println("Ingrese su nombre: ");
-nombre = lectura.readLine();
-apellido= lectura.readLine();
-nickname= lectura.readLine();
+//BufferedReader lectura = new BufferedReader(new InputStreamReader(System.in));
+//String nombre, apellido, nickname;
+//int clave;
+////JCalendar fechNacimiento;
+//
+//System.out.println("Bienvenidos a IDomino");
+//System.out.println("Ingrese su nombre: ");
+//nombre = lectura.readLine();
+//apellido= lectura.readLine();
+//nickname= lectura.readLine();
 //clave= lectura.readLine();
 //fechaNacimiento= lectura.readLine();
 
+//
+//System.out.println("Ingrese su apellido: ");
+//
+//System.out.println("Bienvenido "+nombre);
+//
+//System.out.println("Por favor ");
 
-System.out.println("Ingrese su apellido: ");
 
-System.out.println("Bienvenido "+nombre);
+    Vector <Usuario> lecturaXmlUsuario()
+    {
+            Vector <Usuario> VUsuario= new Vector();
+        try {
+            SAXBuilder builder=new SAXBuilder(false);
+            Document doc = (Document) builder.build("ListaUsuario.xml");
+            Element raiz = doc.getRootElement();
+            List servicios = raiz.getChildren("Usuario");
+            Iterator i = servicios.iterator();
 
-System.out.println("Por favor ");
+           while (i.hasNext()){
+                Element e= (Element)i.next();
+                Element Nombre = e.getChild("Nombre");
+                Element Apellido = e.getChild("Apellido");
+                Element Fechanacimiento = e.getChild("fechanacimiento");
+                Element clave = e.getChild("Clave");
+                Element nickname= e.getChild("nickname");
+                Usuario Aux= new Usuario(Nombre.getValue(),Apellido.getValue(),Fechanacimiento.getValue(),clave.getValue(),nickname.getValue());
+                VUsuario.addElement(Aux);
+//                System.out.println("Datos del Usuario: Nombre "+ Nombre.getValue()+
+//                " Apellido: "+Apellido.getValue()+" Fechanacimiento: "+Fechanacimiento.getValue()+
+//                "Clave: "+ clave.getValue()+ "nickname: "+ nickname.getValue());
+
+           }
+        }catch (Exception e){
+         e.printStackTrace();
+       }
+            return VUsuario;
+   }
 
 
 
