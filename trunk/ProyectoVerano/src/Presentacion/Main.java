792aa1fd5c1eff96322c82b2762dfc3796c1569f
gperez10 <gperez10@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-05 16:56:40 +0000

    cambio de nombre de carpetas a  PRESENTACION

---
+++ trunk/ProyectoVerano/src/Presentacion/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentacion;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+/**
+ *
+ * @author Isra
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new VentanaLogin().setVisible(true);
+    }
+
+}
