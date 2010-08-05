7da15454b2803f31258ebc940d67527d3279b7be
gperez10 <gperez10@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-05 16:56:18 +0000

    cambio de nombre de carpetas a DATO LOGICO PRESENTACION

---
+++ trunk/ProyectoVerano/src/Logico/Imprimiralgo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Logico;
+
+/**
+ *
+ * @author Minerva Verdugo Ruiz
+ */
+public class Imprimiralgo {
+
+    public void prueba () {
+
+    System.out.println("probando codigo para saber si subimos bien, Javy");
+    System.out.println("Holaaa s israel probando subversioning!");
+    System.out.println("Holaaa es MINE");
+    System.out.println("probando que a minerva no le bajan las carpetas");
+    }
+}
---
+++ trunk/ProyectoVerano/src/Logico/ListaUsuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Logico;
+
+/**
+ *
+ * @author Minerva Verdugo Ruiz
+ */
+public class ListaUsuario {
+Usuario elusuario [];
+
+
+public void OrdenarNumPartida (Usuario U){
+
+}
+}
