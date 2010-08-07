dfcb7b213614df611895697e307938098c96518d
gperez10 <gperez10@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-07 21:48:46 +0000

    Clase RegistroUsuario. Recepcion de datos y agregacion en la ListaUsuarios

--- ProyectoVerano/src/Logica/RegistroUsuario.java
+++ ProyectoVerano/src/Logica/RegistroUsuario.java
@@ -4,14 +4,41 @@
  */
 
 package Logica;
-import Presentacion.VentanaLogin;
 
+import Dato.ListaUsuario;
+import Dato.Usuario;
+import javax.swing.JTextField;
 /**
  *
- * @author Minerva Verdugo Ruiz
+ * @author Javy
  */
 public class RegistroUsuario {
 
+    private static ListaUsuario ListaUsuariosRegistrados;
+    private Usuario NuevoUsuario=null;
+
+      public static boolean inicializarEstructuras(){
+        ListaUsuariosRegistrados = new ListaUsuario();
+
+//        ListaUsuariosRegistrados.agregarElemento(new Usuario("Ricardonick", "Casanova", "Profesor"));
+//        ListaUsuariosRegistrados.agregarElemento(new Usuario("Glorianick", "Tarrio", "Coordinadora"));
+//        ListaUsuariosRegistrados.agregarElemento(new Usuario("Susananick", "Garcia", "Directora"));
+//        ListaUsuariosRegistrados.agregarElemento(new Usuario("Evelenirnick", "Barreto", "Profesora"));
+//        ListaUsuariosRegistrados.agregarElemento(new Usuario("Evelenirnick", "Barreto", "Profesora"));
+//        ListaUsuariosRegistrados.imprimirColeccion();
+//        ListaUsuariosRegistrados.removerElemento(new Usuario("Ricardonick", "Casanova", "Profesor"));
+
+        ListaUsuariosRegistrados.ImprimirColeccion();
+        return(true);
+    }
 
 
-}
+      public static void AgregarUsuario(JTextField nickname, JTextField clave, JTextField nombre, JTextField apellido,String avatar){
+    
+      }
+          
+            //NuevoUsuario=new Usuario(nickname.getText(),clave.getText(),nombre.getText(),apellido.getText(),avatar);
+             //ListaUsuariosRegistrados.AgregarElemento(NuevoUsuario);
+      }
+
+ 
