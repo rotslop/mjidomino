3c5ca3b5bb3f2fc4c408f01952e68bcdb43938b9
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-08 17:15:23 +0000

    Modifique aqui lo que seia la lectura del archivo Xml

--- ProyectoVerano/src/Dato/ArchivoXml.java
+++ ProyectoVerano/src/Dato/ArchivoXml.java
@@ -1,9 +1,58 @@
 package Dato;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.*;
+import org.jdom.output.XMLOutputter;
 
 
-class ArchivoXml {
 
+class ArchivoXml{
+
+
+
+
+Vector <Usuario> lecturaXmlUsuario()
+    {
+            Vector <Usuario> VUsuario= new Vector();
+        try {
+            SAXBuilder builder=new SAXBuilder(false);
+            Document doc = (Document) builder.build("ListaUsuario.xml");
+            Element raiz = doc.getRootElement();
+            List servicios = raiz.getChildren("Usuario");
+            Iterator i = servicios.iterator();
+
+           while (i.hasNext()){
+                Element e= (Element)i.next();
+                Element Nombre = e.getChild("Nombre");
+                Element Apellido = e.getChild("Apellido");
+                Element Fechanacimiento = e.getChild("fechanacimiento");
+                Element clave = e.getChild("Clave");
+                Element nickname= e.getChild("nickname");
+                Usuario Aux= new Usuario(Nombre.getValue(),Apellido.getValue(),Fechanacimiento.getValue(),clave.getValue(),nickname.getValue());
+                VUsuario.addElement(Aux);
+
+
+                System.out.println("Datos del Usuario: Nombre "+ Nombre.getValue().toString()+
+                " Apellido: "+Apellido.getValue().toString()+" Fechanacimiento: "+Fechanacimiento.getValue().toString()+
+                "Clave: "+ clave.getValue().toString()+ "nickname: "+ nickname.getValue().toString());
+
+           }
+        }catch (Exception e){
+         e.printStackTrace();
+       }
+
+
+            return VUsuario;
+   }
 
 
 }
+
+
