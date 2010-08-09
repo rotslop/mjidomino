411a87f798b948e446b5e23a3c0c701c4400c936
osuna.isra <osuna.isra@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-09 16:35:40 +0000

    prueba color fondo

--- ProyectoVerano/src/Presentacion/VentanaLogin.java
+++ ProyectoVerano/src/Presentacion/VentanaLogin.java
@@ -58,7 +58,7 @@ private Jcalendar fechanaci;
         jlTituloiDomino = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(0, 0, 0));
+        setBackground(new java.awt.Color(1, 1, 1));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 formWindowClosing(evt);
