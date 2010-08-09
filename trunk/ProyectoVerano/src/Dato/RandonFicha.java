d5a5346cce9e72887b280a66e3dad5510bc7a046
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-09 16:01:56 +0000

    listaficha

--- trunk/ProyectoVerano/src/Dato/RandonFicha.java
+++ trunk/ProyectoVerano/src/Dato/RandonFicha.java
@@ -17,17 +17,37 @@ public class RandonFicha {
 int n;
 
 public void RandonFicha(){
-Random generator = new Random();
-int randomIndex = generator.nextInt( 6);
 
-for (int i=1; i<7; i++){
-    System.out.println(randomIndex + "/");
+    Ficha ficharandom= new Ficha();
+    Random generator2 = new Random();
+    int randomIndex2;
+
+
+    for (int i=1; i<29; i++){
+
+        ficharandom.CrearFichas().get(i);
+        randomIndex2 = generator2.nextInt( 6);
+
+
+    }
+
+//shuffle
+
+
+//    int randomIndex = generator.nextInt( 6);
+//
+//Random generator2 = new Random();
+//int randomIndex2 = generator2.nextInt( 6);
+//
+//for (int i=1; i<7; i++){
+//    System.out.println(randomIndex + "/");
+//}
+//
+//
+//
+//
+//
+//}
+
 }
-
-
-
-
-
-}
-
 }
