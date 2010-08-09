a83861c76b1c9bb9604e302a277479b9d2e986e5
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-09 00:52:01 +0000

    la clase ficha se crean las fichas del domino

--- ProyectoVerano/src/Dato/Ficha.java
+++ ProyectoVerano/src/Dato/Ficha.java
@@ -1,90 +1,3 @@
-
-///*
-// * To change this template, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//
-//package proyectoverano;
-//import java.util.*;
-///**
-// *
-// * @author Minerva Verdugo Ruiz
-// */
-//public class Ficha {
-//
-// private int fichaServidor;
-// private int fichaMaquina;
-// private ListaFicha listficha[];
-// private ListaFichaUsuario listusuario[];
-//
-//    public Ficha(int fichaServidor, int fichaMaquina, ListaFicha[] listficha, ListaFichaUsuario[] listusuario) {
-//        this.fichaServidor = fichaServidor;
-//        this.fichaMaquina = fichaMaquina;
-//        this.listficha = listficha;
-//        this.listusuario = listusuario;
-//    }
-//
-//    public int getFichaMaquina() {
-//        return fichaMaquina;
-//    }
-//
-//    public void setFichaMaquina(int fichaMaquina) {
-//        this.fichaMaquina = fichaMaquina;
-//    }
-//
-//    public int getFichaServidor() {
-//        return fichaServidor;
-//    }
-//
-//    public void setFichaServidor(int fichaServidor) {
-//        this.fichaServidor = fichaServidor;
-//    }
-//
-//    public ListaFicha[] getListficha() {
-//        return listficha;
-//    }
-//
-//    public void setListficha(ListaFicha[] listficha) {
-//        this.listficha = listficha;
-//    }
-//
-//    public ListaFichaUsuario[] getListusuario() {
-//        return listusuario;
-//    }
-//
-//    public void setListusuario(ListaFichaUsuario[] listusuario) {
-//        this.listusuario = listusuario;
-//    }
-//
-//public void MoverFicha (){
-//    RandonFicha R= new RandonFicha();
-//
-//}
-//
-//public static class CrearFicha {
-//
-// public   void main(String[] args) {
-//
-//   Set s = new HashSet();
-//
-//   for(int i=0; i<=6;i++){
-//     for(int j=0; j<=6;j++)
-//     System.out.println(args[i]+","+args[j]);
-//   }
-//
-// }
-//}
-////public void CrearFicha (){
-//
-//
-//
-//
-//
-//
-//
-//
-////}
-//}=======
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -92,80 +5,57 @@
 
 package Dato;
 import java.util.*;
-//import Lista.ListaFicha;
-//import Lista.ListaFichaUsuario;
-
 /**
  *
  * @author Minerva Verdugo Ruiz
  */
 public class Ficha {
 
- private int fichaServidor;
- private int fichaMaquina;
- private ListaFicha listficha[];
- private ListaFicha listusuario[];
+ private int x; //parte inferior
+ private int y;//  parte superior
 
-    public Ficha(int fichaServidor, int fichaMaquina, ListaFicha[] listficha, ListaFicha[] listusuario) {
-        this.fichaServidor = fichaServidor;
-        this.fichaMaquina = fichaMaquina;
-        this.listficha = listficha;
-        this.listusuario = listusuario;
-    }
-
-
-
-    public int getFichaMaquina() {
-        return fichaMaquina;
-    }
-
-    public void setFichaMaquina(int fichaMaquina) {
-        this.fichaMaquina = fichaMaquina;
-    }
-
-    public int getFichaServidor() {
-        return fichaServidor;
-    }
-
-    public void setFichaServidor(int fichaServidor) {
-        this.fichaServidor = fichaServidor;
-    }
-
-    public ListaFicha[] getListficha() {
-        return listficha;
-    }
-
-    public void setListficha(ListaFicha[] listficha) {
-        this.listficha = listficha;
-    }
-
-    public ListaFicha[] getListusuario() {
-        return listusuario;
-    }
-
-    public void setListusuario(ListaFicha[] listusuario) {
-        this.listusuario = listusuario;
-    }
-
-public void MoverFicha (){
-    RandonFicha R= new RandonFicha();
-
-}
-
-public static class CrearFicha {
-
- public   void main(String[] args) {
-
-   Set s = new HashSet();
-
-   for(int i=0; i<=6;i++){
-     for(int j=0; j<=6;j++)
-     System.out.println(args[i]+","+args[j]);
-   }
-
+ public Ficha (){
+     this.x=0;
+     this.y=0;
  }
-}
-//public void CrearFicha (){
+
+    public Ficha(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+ public Vector <Ficha>  CrearFichas (){
+ 
+ Vector <Ficha> Fichas = new Vector();
+
+for (int i=0;i<7;i++){
+for(int j=i;j<7;j++){
+
+Ficha Aux= new Ficha(i,j);
+Fichas.addElement(Aux);
 
 }
-//>>>>>>> .r23
+}
+return Fichas;
+}
+
+}
+
+
+
