fa26f6301783c138d4ea8b15874f631707964733
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-10 01:11:28 +0000

    clase donde inserto el menu para seleccionar alguna opcion

--- ProyectoVerano/src/Logica/ClienteServidor.java
+++ ProyectoVerano/src/Logica/ClienteServidor.java
@@ -10,39 +10,68 @@ package Logica;
  * @author Minerva Verdugo Ruiz
  */
 public class ClienteServidor  {
+    Reserva reserva;
 
 
     public void ClienteServidor(int PM){
     switch (PM){
-//
-//        case 1:
-//        IngresoSistema(PM);
-//        break;
-//        case 2:
-//        CreaPartida (PM);
-//        break;
-//        case 3:
-//        RegistroUsuario R= new RegistroUsuario;
-//        RegistroUsuario(PM);
-//        R.ValidarRegistro(U);
-//        break;
-//        case 4:
-//        GuardaPartida(PM)
-//        break;
-//        case 5:
-//        EnviaJugada(PM)
-//        break;
-//        case 6:
-//        FinPartida(PM);
-//        break;
-//
-//
-//
 
-
-
+        case 1:
+        IngresoSistema(PM);
+        break;
+        case 2:
+        CreaPartida (PM);
+        break;
+        case 3:
+        RegistroUsuario R= new RegistroUsuario();
+        RegistroUsuario(PM);
+        //R.ValidarRegistro(U);
+        break;
+        case 4:
+        //GuardaPartida(PM);
+        break;
+        case 5:
+        EnviaJugada(PM);
+        break;
+        case 6:
+        FinPartida(PM);
+        break;
 
     }
+
+    }
+
+
+
+
+
+
+    public void IngresoSistema(int PM)
+    {
+
+    }
+        public void CreaPartida (int PM)
+    {
+
+    }
+
+
+       public void RegistroUsuario(int PM)
+    {
+
+    }
+
+        public void GuardaPartida(int PM)
+    {
+
+        }
+        public void EnviaJugada(int PM)
+
+    {
+
+        }
+        public void FinPartida(int PM)
+    {
 
     }
 }
