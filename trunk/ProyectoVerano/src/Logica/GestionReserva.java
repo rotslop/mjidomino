d9c03bb2da54d94e93995a040057c170901942da
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-10 00:00:06 +0000

    se realiza la implementacion de la clase GestionReserva

---
+++ trunk/ProyectoVerano/src/Logica/GestionReserva.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Logica;
+
+import ve.edu.ucab.server.logica.LogicaServidor;
+/**
+ *
+ * @author Minerva Verdugo Ruiz
+ */
+public class GestionReserva {
+
+public class GestionReserva implements LogicaServidor {
+
+public String realizarOperacion(String arg) {
+
+String[] subArg = arg.split(":");
+Reserva reserva = new Reserva();
+reserva.setId(Integer.parseInt(subArg[0]));
+reserva.setNombre(subArg[1]);
+
+return realizarReserva(reserva);
+}
+public String realizarReserva(Reserva reserva) {
+String resultado = "TRUE";
+System.out.println("La reserva " + reserva.getNombre() + " se ha efectuado");
+return resultado;
+}
+}
+}
