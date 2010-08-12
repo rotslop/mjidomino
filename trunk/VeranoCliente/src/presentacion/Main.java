044d98dcb4abc37b70fdf76698152eba5e4ce753
gperez10 <gperez10@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-12 22:17:44 +0000

    creando proyecto cliente

---
+++ trunk/VeranoCliente/src/presentacion/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentacion;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import Logica.Comunicacion;
+import Logica.Datos;
+
+//import Logica.Comunicacion;
+/**
+ *
+ * @author Isra
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Comunicacion.solicitarInicializarEstruturas();
+        Comunicacion.solicitarCargaArchivo();
+        Comunicacion.ImprimirListaUsuario();
+
+        Datos.IniciarPartidaNueva("javiccs","123");
+        Datos.ImprimirListaFichas();
+        Datos.IniciarPartidaNueva("javiccs","12");
+        Datos.solicitarGuardarArchivoPartida();
+        Datos.ImprimirListapartidas();
+//        Datos.ImprimirListaFichas();
+//        Comunicacion.solicitarGuardarPartida();
+
+
+
+
+
+
+
+        //new VentanaLogin().setVisible(true);
+         //Comunicacion.solicitarGuardarArchivo();
+    }
+
+}
