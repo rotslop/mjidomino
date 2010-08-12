23f3ca325804f8d5a5ae44d581ee12e2a2d76240
minervaverdugo3 <minervaverdugo3@c1181982-9de1-b6c3-5195-8dc6d4b118ce>
2010-08-12 19:56:12 +0000

    mensaje del titulo

---
+++ trunk/ProyectoVerano/src/cliente/dato/ClienteTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cliente.dato;
+import ve.edu.ucab.cliente.ClienteSocket;
+/**
+ *
+ * @author Minerva Verdugo Ruiz
+ */
+
+//
+public class ClienteTest {
+
+
+public static void main(String[] args) {
+
+ClienteSocket cliente = new ClienteSocket();
+//String resultado = cliente.ejecutarPeticion("1:MINE", "localhost",7687);
+//System.out.println(resultado);
+
+
+
+}
+}
+
+
+
+
